405fc20f6a0dd8abfa626143da7f541563aa91f2
Blazej Leman <blazejleman789@gmail.com>
2023-11-03 02:30:34 +0100

    add string and addword

--- stl.cpp
+++ stl.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 void addSth(std::string table, char sign) {
-    table += sign;
+    std::string table2 = table += sign;
+    std::cout << table2;
 }
 
 void writeString() {
     std::string word = "abcd";
     std::cout << word << std::endl;
     addSth(word,'e');
-    std::cout << word << std::endl;
 }
 
 int main() {
