5ecf8706e20a49bd4576d019924b5bf44c76682f
Blazej Leman <blazejleman789@gmail.com>
2023-11-03 02:23:49 +0100

    add table and addSign

--- stl.cpp
+++ stl.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+void addSth(std::string table, char sign) {
+    table += sign;
+}
+
+void writeString() {
+    std::string word = "abcd";
+    std::cout << word << std::endl;
+    addSth(word,'e');
+    std::cout << word << std::endl;
+}
+
 int main() {
-    
+    writeString();
+    return 0;
 }
