df89e2e00a93df582298dae5b21f089e6f7a19eb
Blazej Leman <blazejleman789@gmail.com>
2023-11-03 02:39:45 +0100

    add returnWord

--- stl.cpp
+++ stl.cpp
@@ -2,16 +2,20 @@
 
 void addSth(std::string table, char sign) {
     std::string table2 = table += sign;
-    std::cout << table2;
+    std::cout << table2 << std::endl;
 }
 
-void writeString() {
-    std::string word = "abcd";
-    std::cout << word << std::endl;
-    addSth(word,'e');
+void writeString(std::string word, char sign) {
+    addSth(word,sign);
+}
+
+void returnWord(std::string word, int place) {
+    std::cout << word[place] << std::endl;
 }
 
 int main() {
-    writeString();
+    std::string word = "abcd";
+    writeString(word,'a');
+    returnWord(word, 3);
     return 0;
 }
